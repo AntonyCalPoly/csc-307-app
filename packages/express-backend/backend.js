5a879e84bd2ca5b58b50ad9d3420666667ee20c1
AntonyCalPoly <atartako@calpoly.edu>
2025-04-22 17:05:05 -0700

    implemented task 2 of IA4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,6 +1,7 @@
 // backend.js
 import express from "express";
 import cors from "cors";
+import userServices from "./user-services.js";
 
 const app = express();
 const port = 8000;
@@ -8,115 +9,44 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
-
-const findUserByNameAndJob = (name, job) => {
-  return users["users_list"].filter((user) => {
-    if (name && job) {
-     return user["name"] === name && user["job"] === job;
-    } else if (name) {
-      return user["name"] === name;
-    } else if (job) {
-      return user["job"] === job;
-    } else {
-      return true;
-    }
-  });
-};
-
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
-
-const generateId = (length = 6) => {
-  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    result += characters.charAt(randomIndex);
-  }
-  
-  return result;
-}
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/users", (req, res) => {
-  const name = req.query.name;
-  const job = req.query.job
-  let result = findUserByNameAndJob(name, job);
-  if (name == undefined && job == undefined) {
-    res.send(users);
-  } else {
-    res.send({ users_list: result});
-  }
+  const { name, job } = req.query;
+  userServices.getUsers(name, job)
+    .then(users => res.send({ users_list: users }))
+    .catch(err => res.status(500).send(err.message));
 });
 
 app.get("/users/:id", (req, res) => {
-  const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  const id = req.params.id;
+  userServices.findUserById(id)
+    .then(user => {
+      if (!user) res.status(404).send("Resource not found.");
+      else res.send(user);
+    })
+    .catch(err => res.status(500).send(err.message));
+});
+
+app.post("/users", (req, res) => {
+  const user = req.body;
+  userServices.addUser(user)
+    .then(savedUser => res.status(201).send(savedUser))
+    .catch(err => res.status(400).send(err.message));
 });
 
 app.delete("/users/:id", (req, res) => {
-  const id = req.params["id"];
-  const index = users["users_list"].findIndex(user => user.id === id);
-  if (index === -1) {
-    res.status(404).send("Resource not found.");
-  } else {
-    users["users_list"].splice(index, 1);
-    res.status(204).send();
-  }
-})
-
-app.post("/users", (req, res) => {
-  const userToAdd = req.body;
-  userToAdd.id = generateId(6);
-  addUser(userToAdd);
-  res.status(201).send(userToAdd);
+  const id = req.params.id;
+  userServices.findUserById(id)
+    .then(user => {
+      if (!user) return res.status(404).send("Resource not found.");
+      return user.deleteOne().then(() => res.status(204).send());
+    })
+    .catch(err => res.status(500).send(err.message));
 });
 
 app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
+  console.log(`Server running at http://localhost:${port}`);
 });
